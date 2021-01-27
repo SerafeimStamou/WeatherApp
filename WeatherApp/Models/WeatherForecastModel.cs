e313d962f46258d0fd6a8f8e53866df9accaca5f
SerafeimStamou <serafeim_stamou86@hotmail.com>
2021-01-27 19:10:09 +0200

    Initial

---
+++ WeatherApp/Models/WeatherForecastModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace WeatherApp
+{
+    public class WeatherForecastModel
+    {
+        public DayForecast[] Consolidated_weather { get; set; }
+        public DateTime Sun_rise { get; set; }
+        public DateTime Sun_set { get; set; }
+        public string Title { get; set; }
+        public string Timezone { get; set; }
+    }
+}
